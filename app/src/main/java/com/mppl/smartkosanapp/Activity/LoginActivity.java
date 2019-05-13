8a26d808c31c6ce25f3a63e85a5cf313b38b579d
Ary Sugiarto <arysugiarto10@gmail.com>
2019-05-14 05:36:17 +0700

    added login screen & home screen (proses)

---
+++ app/src/main/java/com/mppl/smartkosanapp/Activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.mppl.smartkosanapp.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mppl.smartkosanapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
