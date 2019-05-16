44214d6271315c4239448cc55d77d4fddced6c13
Ary Sugiarto <arysugiarto10@gmail.com>
2019-05-17 05:06:24 +0700

    add screen tagihan,kamar,histori + perubahan icon

--- app/src/main/java/com/mppl/smartkosanapp/Activity/MainActivity.java
+++ app/src/main/java/com/mppl/smartkosanapp/Activity/MainActivity.java
@@ -2,52 +2,72 @@ package com.mppl.smartkosanapp.Activity;
 
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.TextView;
 
+import com.mppl.smartkosanapp.Fragment.HistoriFragment;
+import com.mppl.smartkosanapp.Fragment.HomeFragment;
+import com.mppl.smartkosanapp.Fragment.KamarFragment;
+import com.mppl.smartkosanapp.Fragment.TagihanFragment;
 import com.mppl.smartkosanapp.R;
 
-public class MainActivity extends AppCompatActivity {
-    private TextView mTextMessage;
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener  {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+            loadFragment(new HomeFragment());
+            BottomNavigationView bottomNavigationView = findViewById(R.id.bn_main);
+            bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        }
+
+
 
         @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment fragment = null;
+            switch (menuItem.getItemId()){
+                case R.id.home_menu:
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.tagihan:
+                    fragment = new TagihanFragment();
+                    break;
+                case R.id.kamar:
+                    fragment = new KamarFragment();
+                    break;
+                case R.id.history:
+                    fragment = new HistoriFragment();
+                    break;
+                case R.id.akun:
+                    fragment = new HomeFragment();
+                    break;
             }
-            return false;
+            return loadFragment(fragment);
         }
-    };
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-//        BottomNavigationView navView = findViewById(R.id.nav_view);
-//        mTextMessage = findViewById(R.id.message);
-//        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fl_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 
 }
---
+++ app/src/main/java/com/mppl/smartkosanapp/Fragment/TagihanFragment.java
@@ -0,0 +1,32 @@
+package com.mppl.smartkosanapp.Fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mppl.smartkosanapp.R;
+
+
+public class TagihanFragment extends Fragment {
+
+
+    public TagihanFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tagihan, container, false);
+    }
+
+}
