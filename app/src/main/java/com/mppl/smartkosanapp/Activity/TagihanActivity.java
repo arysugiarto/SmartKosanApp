8ce73f1d7d041e94074747ce5f651d61400e95de
Ary Sugiarto <arysugiarto10>
2019-07-05 15:09:10 +0700

    upload masih eror males neruskeeeun

--- app/src/main/java/com/mppl/smartkosanapp/Activity/TagihanActivity.java
+++ app/src/main/java/com/mppl/smartkosanapp/Activity/TagihanActivity.java
@@ -1,11 +1,38 @@
 package com.mppl.smartkosanapp.Activity;
 
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.mppl.smartkosanapp.R;
+import com.mppl.smartkosanapp.rest.AppController;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,6 +47,27 @@ public class TagihanActivity extends AppCompatActivity {
     String id;
 
 
+    Button buttonChoose;
+    Button buttonUpload;
+    ImageView imageView;
+    EditText txt_name;
+    Bitmap bitmap, decoded;
+    int success;
+    int PICK_IMAGE_REQUEST = 1;
+    int bitmap_size = 60; // range 1 - 100
+
+    private static final String TAG = TagihanActivity.class.getSimpleName();
+
+    /* 10.0.2.2 adalah IP Address localhost Emulator Android Studio. Ganti IP Address tersebut dengan
+    IP Address Laptop jika di RUN di HP/Genymotion. HP/Genymotion dan Laptop harus 1 jaringan! */
+    private String UPLOAD_URL = "http://192.168.1.24/webservice/SmartKosanWebService/upload.php";
+
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+    private String KEY_IMAGE = "image";
+    private String KEY_NAME = "name";
+
+    String tag_json_obj = "json_obj_req";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +81,29 @@ public class TagihanActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
 
+        buttonChoose = (Button) findViewById(R.id.pilih);
+        buttonUpload = (Button) findViewById(R.id.upload);
+
+        txt_name = (EditText) findViewById(R.id.editText);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        buttonChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFileChooser();
+            }
+        });
+
+        buttonUpload.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
+
+
 
         getData();
     }
@@ -44,6 +115,130 @@ public class TagihanActivity extends AppCompatActivity {
 //
         jenis.setText(sjenis);
         tgl.setText(stgl);
+    }
+
+
+    public String getStringImage(Bitmap bmp) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, bitmap_size, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+    private void uploadImage() {
+        //menampilkan progress dialog
+        final ProgressDialog loading = ProgressDialog.show(this, "Uploading...", "Please wait...", false, false);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, UPLOAD_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.e(TAG, "Response: " + response.toString());
+
+                        try {
+                            JSONObject jObj = new JSONObject(response);
+                            success = jObj.getInt(TAG_SUCCESS);
+
+                            if (success == 1) {
+                                Log.e("v Add", jObj.toString());
+
+                                Toast.makeText(TagihanActivity.this, jObj.getString(TAG_MESSAGE), Toast.LENGTH_LONG).show();
+
+                                kosong();
+
+                            } else {
+                                Toast.makeText(TagihanActivity.this, jObj.getString(TAG_MESSAGE), Toast.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        //menghilangkan progress dialog
+                        loading.dismiss();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //menghilangkan progress dialog
+                        loading.dismiss();
+
+                        //menampilkan toast
+                        Toast.makeText(TagihanActivity.this, error.getMessage().toString(), Toast.LENGTH_LONG).show();
+                        Log.e(TAG, error.getMessage().toString());
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                //membuat parameters
+                Map<String, String> params = new HashMap<String, String>();
+
+                //menambah parameter yang di kirim ke web servis
+                params.put(KEY_IMAGE, getStringImage(decoded));
+                params.put(KEY_NAME, txt_name.getText().toString().trim());
+
+                //kembali ke parameters
+                Log.e(TAG, "" + params);
+                return params;
+            }
+        };
+
+        AppController.getInstance().addToRequestQueue(stringRequest, tag_json_obj);
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri filePath = data.getData();
+            try {
+                //mengambil fambar dari Gallery
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                // 512 adalah resolusi tertinggi setelah image di resize, bisa di ganti.
+                setToImageView(getResizedBitmap(bitmap, 512));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void kosong() {
+        imageView.setImageResource(0);
+        txt_name.setText(null);
+    }
+
+    private void setToImageView(Bitmap bmp) {
+        //compress image
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, bitmap_size, bytes);
+        decoded = BitmapFactory.decodeStream(new ByteArrayInputStream(bytes.toByteArray()));
+
+        //menampilkan gambar yang dipilih dari camera/gallery ke ImageView
+        imageView.setImageBitmap(decoded);
+    }
+
+    // fungsi resize image
+    public Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = (float) width / (float) height;
+        if (bitmapRatio > 1) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+        return Bitmap.createScaledBitmap(image, width, height, true);
     }
 
 
